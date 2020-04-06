b24325029cf0a275e4c82b57908743ae160425df
Czeffik <trzewik93@gmail.com>
2020-04-06 22:16:50 +0200

    Add method for resolving jms destination

--- src/main/java/com/trzewik/activemq/infrastructure/jms/information/InformationProducerImpl.java
+++ src/main/java/com/trzewik/activemq/infrastructure/jms/information/InformationProducerImpl.java
@@ -4,6 +4,9 @@ import com.trzewik.activemq.domain.information.InformationProducer;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.command.ActiveMQDestination;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.jms.core.JmsTemplate;
 
 @Slf4j
@@ -19,18 +22,35 @@ class InformationProducerImpl implements InformationProducer {
     @Override
     public void send(String message) {
         log.info("Sending message: [{}], to queue: [{}]", message, queue);
-        jmsTemplate.convertAndSend(queue, message);
+        jmsTemplate.convertAndSend(resolveDestination(queue), message);
     }
 
     @Override
     public void publish(String message) {
         log.info("Publishing message: [{}], on topic: [{}]", message, topic);
-        jmsTemplate.convertAndSend(topic, message);
+        jmsTemplate.convertAndSend(resolveDestination(topic), message);
     }
 
     @Override
     public void publishOnVirtualTopic(String message) {
         log.info("Publishing message: [{}], on topic: [{}]", message, virtualTopic);
-        jmsTemplate.convertAndSend(virtualTopic, message);
+        jmsTemplate.convertAndSend(resolveDestination(virtualTopic), message);
+    }
+
+    //TODO should be moved to DestinationResolverClass  - create some naming convention
+    private static ActiveMQDestination resolveDestination(String destination) {
+        String upperCaseDestination = destination.toUpperCase();
+        if (hasTopic(upperCaseDestination) && hasNotConsumer(upperCaseDestination)) {
+            return new ActiveMQTopic(destination);
+        }
+        return new ActiveMQQueue(destination);
+    }
+
+    private static boolean hasTopic(String destination) {
+        return destination.contains("TOPIC");
+    }
+
+    private static boolean hasNotConsumer(String destination) {
+        return !destination.contains("CONSUMER");
     }
 }
