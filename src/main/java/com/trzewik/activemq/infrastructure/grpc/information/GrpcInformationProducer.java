64effd01a5fadcc1ba53c765fb074fec01b2f62b
Czeffik <trzewik93@gmail.com>
2020-04-06 20:14:22 +0200

    Fix issue with removing element from list while iterating them

--- src/main/java/com/trzewik/activemq/infrastructure/grpc/information/GrpcInformationProducer.java
+++ src/main/java/com/trzewik/activemq/infrastructure/grpc/information/GrpcInformationProducer.java
@@ -5,6 +5,7 @@ import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,25 +16,29 @@ public class GrpcInformationProducer implements StreamInformationProducer {
     private final List<StreamObserver<InformationDTO>> observers = new LinkedList<>();
 
     @Override
-    public void send(String message) {
+    public synchronized void send(String message) {
         log.info("Notifying [{}] observers about new message: [{}]", observers.size(), message);
+        List<StreamObserver<InformationDTO>> inactiveObservers = new ArrayList<>();
         observers.forEach(o -> {
             try {
                 o.onNext(InformationDto.from(message));
             } catch (StatusRuntimeException ex) {
-                log.info("Removing observer because: [{}]", ex.getMessage());
+                log.info("Scheduling observer to remove because: [{}]", ex.getMessage());
                 log.debug(Arrays.toString(ex.getStackTrace()));
-                remove(o);
+                inactiveObservers.add(o);
             }
         });
+        inactiveObservers.forEach(this::remove);
     }
 
     @Override
     public synchronized void add(StreamObserver<InformationDTO> observer) {
+        log.info("Adding new observer");
         this.observers.add(observer);
     }
 
     private synchronized void remove(StreamObserver<InformationDTO> observer) {
+        log.info("Removing observer");
         this.observers.remove(observer);
     }
 }
